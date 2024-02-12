84c05a68a3eeddbc8170e45f08d9f18729325bb4
jbburt <joshuaburtphd@gmail.com>
2024-02-11 19:43:24 -0500

    simplify

--- jburt/llm/agent.py
+++ jburt/llm/agent.py
@@ -2,7 +2,8 @@ import json
 import pathlib
 from collections import deque
 from os import environ as e_
-
+from typing import Any
+from jburt.objects import dotdict
 import openai
 from openai.types.chat import ChatCompletion
 
@@ -21,48 +22,43 @@ class CacheConfig:
     assistant_key = "assistant"
 
 
-class Response(CacheConfig):
+class Response:
     """
 
     Attributes:
-        response (openai.types.ChatCompletion): The response.
-        model (str): The chat model.
+        response         : openai.types.chat.ChatCompletion response object.
+        model       (str): The chat model.
         fingerprint (str): The backend configuration.
-        id (str): A unique identifier for the chat completion.
-        created (int): The time the chat completion was created.
-        role (str): The role assigned to the responder.
-        content (str): The content of the response.
+        id          (str): A unique identifier for the chat completion.
+        created     (int): Unix timestamp (s) of when completion was created.
+        role        (str): The role assigned to the responder.
+        content     (str): The content of the response.
 
     """
 
-    def __init__(self, response):
+    def __init__(self, response: ChatCompletion):
         super().__init__()
-        self.response: ChatCompletion = response
+        self.id: str = response.id
+        self.model: str = response.model
         self.fingerprint: str = response.system_fingerprint
-        self.model = response.model
-        # self.id: str = response.id
         self.role: str = response.choices[0].message.role
         self.content: str = response.choices[0].message.content
-        # self.usage: dict = response.choices[0].message.usage
+        self.created: int = response.created
+        self.usage: dict = response.usage.model_dump_json()
+        self.response = response
 
-    def message(self) -> dict:
-        return {
-            # "model": self.model,
-            # "fingerprint": self.fingerprint,
-            # "id": self.id,
-            # "created": self.created,
-            "role": self.role,
-            "content": self.content,
-        }
+    def to_msg(self) -> dict:
+        return {"role": self.role, "content": self.content}
 
-    def dict(self) -> dict:
+    def to_json(self) -> dict:
         return {
             "model": self.model,
-            "fingerprint": self.fingerprint,
             "id": self.id,
+            "fingerprint": self.fingerprint,
             "role": self.role,
             "content": self.content,
-        }
+            "created": self.created,
+        } | self.usage
 
 
 class Role:
@@ -73,28 +69,33 @@ class MLE(Role):
     assignment = "you are an expert ML engineer and sysadmin.\n---\n"
 
 
-class LMConfig:
-    client = None
-    # gpt model
+class Model:
+    client: Any = None
+    max_hist_len = 10  # max length of convo history
     model = "gpt-4-1106-preview"
-    # max length of convo history
-    nhistbuf = 10
-    # api parameters
-    params_ = {
-        "temperature": 0.0,
-        "max_tokens": 128,
-        "stream": False,
-        "model": model,
-    }
+    temperature = 0.0
+    max_tokens = 128
+    stream = False
 
     def __init__(self, system=MLE.assignment) -> None:
         self.client = openai.OpenAI()
         self.system = system
 
     def create(self, messages, **kwargs):
-        print(messages)
+        kw = self.get_api_params() | kwargs
         return Response(
-            self.client.chat.completions.create(messages=messages, **kwargs)
+            self.client.chat.completions.create(
+                messages=messages,
+                **kw,
+            )
+        )
+
+    def get_api_params(self):
+        return dotdict(
+            model=self.model,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            stream=self.stream,
         )
 
 
@@ -125,47 +126,52 @@ class Cache(CacheConfig):
             write(content + "\n")
 
 
-class Conversation(Cache, LMConfig):
-    messages: deque[dict] = deque(maxlen=LMConfig.nhistbuf)
-
-    def system_message(self) -> dict:
-        return {"role": self.system_key, "content": self.system}
+class Conversation(Cache, Model):
+    _messages: deque[dict] = deque(maxlen=Model.max_hist_len)
 
     @classmethod
     def from_history(cls, history: list[dict]) -> "Conversation":
         self = cls()
-        self.messages = deque(history, maxlen=cls.nhistbuf)
+        self._messages = deque(history, maxlen=cls.max_hist_len)
         return self
 
     def __init__(self):
         super().__init__()
         self.id = str(self.cache_size() + 1).zfill(6)
-        self.messages = deque(maxlen=self.nhistbuf)
-        self.messages.append(self.system_message())
+
+        self._messages = deque(maxlen=self.max_hist_len)
+        self._messages.append(self.system_message())
+
+        self.last_response = None
 
     def send_receive(self, messages, debug=False, **kwargs) -> dict:
-        params = self.params_ | (kwargs or {})
-        response = self.create(messages, **params)
-        self.messages.append(response.message())
-        self.write_to_cache(
-            id=self.id,
-            messages=self.messages,
-            debug=debug,
-        )
+        params = self.get_api_params | (kwargs or {})
+        response = self.last_response = self.create(messages, **params)
+        self._messages.append(response.to_msg())
+        self.write_to_cache(id=self.id, messages=self._messages, debug=debug)
 
     def __call__(self, prompt: str) -> str:
-        self.messages.append({"role": self.user_key, "content": prompt})
-        self.send_receive(list(self.messages))
-        print(self.messages[-1]["content"])
-        return self.messages[-1]["content"]
+        self._messages.append({"role": self.user_key, "content": prompt})
+        self.send_receive(list(self._messages))
+        print(self._messages[-1]["content"])
+        return self._messages[-1]["content"]
+
+    def system_message(self) -> dict:
+        return {"role": self.system_key, "content": self.system}
+
+    @property
+    def messages(self) -> list[dict]:
+        return list(self._messages)
 
 
-class Agent(Conversation):
+# alias
+Convo = Conversation
+
+
+class Agent:
     def __init__(self):
         super().__init__()
 
 
 agent = Agent()
 agent("hello")
-
-agent("bro what did you just say to me")
--- jburt/types.py
+++ jburt/types.py
@@ -4,10 +4,12 @@ from typing import TypeVar
 
 import numpy as np
 
-Shape = TypeVar("Shape")
-DType = TypeVar("DType")
 PathLike = TypeVar("PathLike", str, pathlib.Path)
 Numeric = TypeVar("Numeric", int, float, complex, np.number)
+
+# // Begin src = https://stackoverflow.com/a/64032593
+Shape = TypeVar("Shape")
+DType = TypeVar("DType")
 
 
 class Array(np.ndarray, Generic[Shape, DType]):
@@ -16,8 +18,9 @@ class Array(np.ndarray, Generic[Shape, DType]):
         image: Array['H,W,3', np.uint8]
         xy_points: Array['N,2', float]
         nd_mask: Array['...', bool]
-
-    Copied from https://stackoverflow.com/a/64032593
     """
 
     pass
+
+
+# // End
